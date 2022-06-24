5afb4caf1ecd0c06ac4e074034d2819aad176241
Georgina Adzorgenu <gadzorgenu@gmail.com>
2022-06-24 12:26:11 +0000

    Added user controller

--- src/main/java/com/example/oshopbackend/controller/UserController.java
+++ src/main/java/com/example/oshopbackend/controller/UserController.java
@@ -1,18 +1,14 @@
 package com.example.oshopbackend.controller;
 
 import com.example.oshopbackend.entities.User;
+import com.example.oshopbackend.exceptions.CustomException;
 import com.example.oshopbackend.services.UserService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -26,5 +22,13 @@ public class UserController {
             @ApiParam(value = "User details")
             @RequestBody User user){
         return new ResponseEntity<>(service.createUser(user), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    @ApiOperation("Update user")
+    public ResponseEntity<User> updateUser(
+            @ApiParam(value = "New user details")
+            @RequestBody User userUpdate) throws CustomException {
+        return new ResponseEntity<>(service.updateUser(userUpdate), HttpStatus.OK);
     }
 }
